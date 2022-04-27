7ebd55245da92cc9eb9d169bfea9b30052d7f262
arashimoya <arasimovicadam@gmail.com>
2022-04-27 18:37:16 +0200

    added packages / PS-158

--- app/src/main/java/com/example/sep4android/View/MainActivity.java
+++ app/src/main/java/com/example/sep4android/View/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.sep4android;
+package com.example.sep4android.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.sep4android.R;
 
 public class MainActivity extends AppCompatActivity {
 
