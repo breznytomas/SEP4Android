8d643eab1933b652980ab83a1ce093ae37015435
Suprajski <bartosz.ochedzan@gmail.com>
2022-05-04 17:22:24 +0200

    PS-163 LiveData implemented

--- app/src/main/java/com/example/sep4android/ViewModel/MessageViewModel.java
+++ app/src/main/java/com/example/sep4android/ViewModel/MessageViewModel.java
@@ -1,13 +1,23 @@
 package com.example.sep4android.ViewModel;
 
 import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.LiveData;
 
+import com.example.sep4android.RemoteDataSource.MessageResponse;
 import com.example.sep4android.Repository.MessageRepository;
+
+
+
+import java.util.List;
 
 public class MessageViewModel extends ViewModel {
     private MessageRepository repository;
 
     public MessageViewModel(){
         repository = MessageRepository.getInstance();
+    }
+
+    public LiveData<List<MessageResponse>> getAllMessages() {
+        return repository.getReceivedMessages();
     }
 }
